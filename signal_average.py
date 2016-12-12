9d1e7bd9ba7f6fbb4223c1f7e4c31c026d80780d
Frank <frank.pan@mail.utoronto.ca>
2016-12-11 21:55:08 -0500

    added script to calculate average of signals

---
+++ signal_average.py
@@ -0,0 +1,15 @@
+import numpy
+
+filename = "./data/signal5/signal"
+
+if __name__ == "__main__":
+    for samples in (50, 100, 200, 400, 800, 1600):
+        t_set = []
+        for i in range(samples):
+            t_set.append(numpy.loadtxt(filename + str(i + 1) + ".txt"))
+
+        arr = numpy.array(t_set)
+        print "number of samples:", samples
+        arr_mean = numpy.mean(arr, axis=0)
+        for elem in arr_mean:
+            print elem
